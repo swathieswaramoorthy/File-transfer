bbdaa241ff23231eb148c8b93be4ed4394251c48
swathieswaramoorthy <126444648+swathieswaramoorthy@users.noreply.github.com>
2024-11-15 22:57:50 +0530

    alt2

--- client.py
+++ client.py
@@ -1,25 +1,19 @@
 import socket
 import os
 
-# Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Define the server address and port
 server_address = '127.0.0.1'
 server_port = 12345
 
-# Connect to the server
 client_socket.connect((server_address, server_port))
 
-# Specify the file to send
-filename = 'sample.txt'  # Change this to your file name
+filename = 'sample.txt'  
 
-# Send the file name to the server
 client_socket.send(filename.encode())
 
-# Open the file in read-binary mode
 with open(filename, 'rb') as f:
-    # Read the file data in chunks
+   
     bytes_read = f.read(1024)
     while bytes_read:
         client_socket.send(bytes_read)
@@ -27,5 +21,5 @@ with open(filename, 'rb') as f:
 
 print(f"File {filename} sent successfully.")
 
-# Close the socket
+
 client_socket.close()
